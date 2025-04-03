8f0041358177d2f9ea8ac393591a9ba1efd835ea
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2025-04-03 23:26:26 +0530

    Implement createNote web service

--- back-ends/spring-back-end/src/main/java/lk/ijse/dep13/springbackend/api/NoteHttpController.java
+++ back-ends/spring-back-end/src/main/java/lk/ijse/dep13/springbackend/api/NoteHttpController.java
@@ -1,15 +1,41 @@
 package lk.ijse.dep13.springbackend.api;
 
+import lk.ijse.dep13.springbackend.entity.Note;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @RestController
 @RequestMapping("/notes")
 public class NoteHttpController {
+
+    @Autowired
+    private Connection connection;
+
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public String createNote(){
-        return "Create a new note";
+    @PostMapping(consumes = "application/json")
+    public Note createNote(@SessionAttribute("user") String email, @RequestBody Note note) throws SQLException {
+        if (email == null) throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,
+                "This operation only supports for authenticated users");
+        try(var stm = connection
+                .prepareStatement("INSERT INTO note (text, \"user\", color) VALUES (?, ?, ?)",
+                        Statement.RETURN_GENERATED_KEYS)){
+            stm.setString(1, note.getText());
+            stm.setString(2, email);
+            stm.setString(3, note.getColor());
+            stm.executeUpdate();
+            ResultSet rst = stm.getGeneratedKeys();
+            rst.next();
+            int id = rst.getInt(1);
+            note.setId(id);
+            return note;
+        }
     }
 
     @GetMapping
@@ -23,14 +49,14 @@ public class NoteHttpController {
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PatchMapping("/{id:^\\d+$}")
-    public String updateNote(@PathVariable Integer id){
-        return "Update note " + id;
-    }
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id:^\\d+$}")
     public String deleteNote(@PathVariable Integer id){
         return "Delete note " + id;
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{id:^\\d+$}")
+    public String updateNote(@PathVariable Integer id){
+        return "Update note " + id;
     }
 }
